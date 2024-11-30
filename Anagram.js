0d2cc5eaf41bfc965f9db3bef32641c974ec5150
Jay Ramoliya <jankipateljp03@gmail.com>
2024-11-30 16:03:18 +0530

    Minimize the Heights II

--- Anagram.js
+++ Anagram.js
@@ -1,4 +1,4 @@
-// Anagram
+// Anagram (30-11-2024)
 
 // Given two strings s1 and s2 consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
 
